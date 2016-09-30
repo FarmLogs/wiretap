eb13266a549b2be93fd69046e1b180cd7ea9bd34
Damian Wieczorek <damian@farmlogs.com>
2016-09-30 13:08:15 -0400

    remove useless comments, replace with doc comments where applicable

--- wiretap-example/src/main/java/com/example/wiretap/WiretapApplication.java
+++ wiretap-example/src/main/java/com/example/wiretap/WiretapApplication.java
@@ -4,11 +4,6 @@ import android.app.Application;
 
 import com.farmlogs.wiretap.runtime.Wiretap;
 
-/**
- * @author Damian Wieczorek {@literal <damian@farmlogs.com>}
- * @since 9/30/16
- * (C) 2016 Damian Wieczorek
- */
 public class WiretapApplication extends Application {
 
   @Override
